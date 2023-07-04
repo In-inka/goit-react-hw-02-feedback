fbdeed3e275056864a3e54cffc7e3e86fe141340
Inna Shkilna <shkinai.1995@gmail.com>
2023-07-04 13:50:20 +0300

    create name styled component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
 import Section from './Section';
 import Notification from './Notification';
-import { App } from './App.styled';
+import { Application } from './App.styled';
 
 export class App extends Component {
   state = {
@@ -38,7 +38,7 @@ export class App extends Component {
     const total = this.countTotalFeedback();
 
     return (
-      <App>
+      <Application>
         <Section title="Please leave feedback">
           <FeedbackOptions
             options={options}
@@ -58,7 +58,7 @@ export class App extends Component {
             <Notification message="There is no feedback" />
           )}
         </Section>
-      </App>
+      </Application>
     );
   }
 }
